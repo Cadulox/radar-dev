1a108049d49297e80b3c1b03ec0234586bbdb28c
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-01-15 11:53:39 -0300

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+// MongoDB (Não-relacional)
+// Conexão com o banco de dados mongoDB
+mongoose.connect('mongodb+srv://omnistack:omnistack1234@cluster0-dobcb.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
---
+++ backend/src/routes.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
+
+const routes = Router();
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: request.query (Filtros, ordenação, paginação, ...) -> GET
+// Rout Params: request.params (Identificar um recurso na alteração, ou remoção) -> DELETE
+// Body: request.body (Dados para criação ou alteração de um registro) -> POST, PUT
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+
+routes.get('/search', SearchController.index);
+
+module.exports = routes;
